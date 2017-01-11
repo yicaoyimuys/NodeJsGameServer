6b9571314ccc74573135763ebafff7d1492f7d57
yangsong <yicaoyimuys@github.com>
2017-01-11 15:12:15 +0800

    数据库添加last_scene_id字段

--- config/game_db.sql
+++ config/game_db.sql
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `money` int(11) NOT NULL,
   `create_time` int(11) NOT NULL COMMENT '注册时间',
   `last_login_time` int(11) NOT NULL COMMENT '最后登录时间',
+  `last_scene_id` int(11) NOT NULL COMMENT '最后所在场景',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
